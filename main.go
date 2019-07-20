85d12e640a9991aaf25ceeedde0fece9d1e36840
Mikita Kryvitski <solairerove@gmail.com>
2019-07-20 21:23:34 +0300

    feature: just save through domain model

--- main.go
+++ main.go
@@ -4,9 +4,8 @@ import (
 	"database/sql"
 	"fmt"
 
-	uuid "github.com/satori/go.uuid"
-
 	_ "github.com/lib/pq"
+	domain "github.com/solairerove/linden-honey-go-scrapper/domain"
 )
 
 // TODO  to properties
@@ -28,28 +27,18 @@ func main() {
 	}
 	defer db.Close()
 
-	// song := domain.Song{
-	// 	Title:  "Something",
-	// 	Link:   "pff",
-	// 	Author: "Letov",
-	// 	Album:  "Experiments",
-	// 	Verses: []domain.Verse{
-	// 		domain.Verse{
-	// 			Ordinal: 1,
-	// 			Data:    "?",
-	// 		},
-	// 	},
-	// }
-
-	var id uuid.UUID
-	err = db.QueryRow(`INSERT INTO songs(title, link, author, album) 
-											VALUES($1, $2, $3, $4) 
-											RETURNING id`,
-		"s.Title", "s.Link", "s.Author", "s.Album").Scan(&id)
-
-	if err != nil {
-		fmt.Println(err)
+	song := domain.Song{
+		Title:  "Something",
+		Link:   "pff",
+		Author: "Letov",
+		Album:  "Experiments",
+		Verses: []domain.Verse{
+			domain.Verse{
+				Ordinal: 1,
+				Data:    "?",
+			},
+		},
 	}
 
-	fmt.Println(id)
+	song.SaveSong(db)
 }
