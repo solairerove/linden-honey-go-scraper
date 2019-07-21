69ccae6e7991b64287450ccfda50cb21d28a6fdf
Mikita Kryvitski <solairerove@gmail.com>
2019-07-21 13:18:44 +0300

    feature: prepare to die edition

--- main.go
+++ main.go
@@ -1,31 +1,30 @@
 package main
 
 import (
-	"database/sql"
-	"fmt"
-
 	_ "github.com/lib/pq"
 	scrapper "github.com/solairerove/linden-honey-go-scrapper/scrapper"
 )
 
 // TODO  to properties
-const (
-	dbUsername = "linden-honey-user"
-	dbPassword = "linden-honey-pass"
-	dbName     = "linden-honey"
-	dbPort     = "5430"
-)
+// const (
+// 	dbUsername = "linden-honey-user"
+// 	dbPassword = "linden-honey-pass"
+// 	dbName     = "linden-honey"
+// 	dbPort     = "5430"
+// )
 
 func main() {
 
-	dbinfo := fmt.Sprintf("user=%s password=%s dbname=%s port=%s sslmode=disable",
-		dbUsername, dbPassword, dbName, dbPort)
+	// dbinfo := fmt.Sprintf("user=%s password=%s dbname=%s port=%s sslmode=disable",
+	// 	dbUsername, dbPassword, dbName, dbPort)
 
-	db, err := sql.Open("postgres", dbinfo)
-	if err != nil {
-		fmt.Println(err)
-	}
-	defer db.Close()
+	// db, err := sql.Open("postgres", dbinfo)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+	// defer db.Close()
 
-	scrapper.ScrapLetov(db)
+	// scrapper.ScrapLetov(db)
+
+	scrapper.ScrapLetov2()
 }
---
+++ scrapper/scrapper2.go
@@ -0,0 +1,10 @@
+package scrapper
+
+import (
+	"fmt"
+)
+
+// ScrapLetov2 is a colly scrapper of grob 2.0
+func ScrapLetov2() {
+	fmt.Println("ScrapLetov2")
+}
