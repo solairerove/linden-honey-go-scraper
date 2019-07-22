e0b5954870d4f5cc8fb41bdf73e87649f082d061
Mikita Kryvitski <solairerove@gmail.com>
2019-07-22 19:47:50 +0300

    feature: add gorrila mux dep

--- main.go
+++ main.go
@@ -1,9 +1,23 @@
 package main
 
 import (
-	scraper "github.com/solairerove/linden-honey-go-scrapper/scraper"
+	"fmt"
+	"html"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	scraper.ScrapLetov()
+	// scraper.ScrapLetov()
+
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", index)
+
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
 }
